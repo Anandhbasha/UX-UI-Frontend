4a41491f3db471a0feda8c34db36a42a9d0d51b2
Anandhbasha <anadhakm@gmail.com>
2025-09-24 12:13:28 +0530

    js

---
+++ Bootstrap/main.js
@@ -0,0 +1,33 @@
+// Hoisting
+// console.log("Hello this JavaScript");
+// var
+// var newText = "Hello this JavaScript"
+// console.log(newText);
+
+// console.log(a);
+var a=10
+var a=60
+a=70
+console.log(a);
+
+// console.log(a);
+
+// let
+
+let b=20
+b=60
+console.log(b);
+
+// const
+const c=44
+// c=88
+console.log(c);
+
+// block scope 
+// global scope 
+
+{
+    const d = 80
+}
+console.log(d);
+

b02b76fa074535c1e46c27d31e696ada607bde0d
Anandhbasha <anadhakm@gmail.com>
2025-09-25 11:50:05 +0530

    js

--- Bootstrap/main.js
+++ Bootstrap/main.js
@@ -4,30 +4,92 @@
 // var newText = "Hello this JavaScript"
 // console.log(newText);
 
-// console.log(a);
-var a=10
-var a=60
-a=70
-console.log(a);
-
+// // console.log(a);
+// var a=10
+// var a=60
+// a=70
 // console.log(a);
 
-// let
+// // console.log(a);
 
-let b=20
-b=60
-console.log(b);
+// // let
 
-// const
-const c=44
-// c=88
-console.log(c);
+// let b=20
+// b=60
+// console.log(b);
 
-// block scope 
-// global scope 
+// // const
+// const c=44
+// // c=88
+// console.log(c);
 
-{
-    const d = 80
-}
-console.log(d);
+// // block scope 
+// // global scope 
+
+// {
+//     const d = 80
+// }
+// console.log(d);
+
+// datatypes
+// premitive
+    let a = 20
+    let userName = "xyz"
+    let todayClassAvalible = true
+    console.log(typeof(todayClassAvalible));
+    
+    console.log(typeof(userName));
+    let b = undefined
+    console.log(typeof(b));
+    let c =null
+    console.log(typeof(c));
+    
+// non premitive
+    // array
+    let arr = [20,"xyz",true,undefined] //index = 0
+    console.log(arr);
+    console.log(arr[0]);
+    console.log(arr[1]);
+    console.log(arr[2]);
+    console.log(arr[3]);
+    
+    
+    // object
+    let pesron ={
+        userName:"xyz",
+        age:28,
+        city:"CBE",
+        family:{
+            dad:"nbjknnk",
+            mom:"tthkjhkjljkl",
+            siblings:{
+                brother:"ertyfhjghj",
+                sister:"nbvghggvn"
+            }
+        }
+    }
+    console.log(pesron.userName);
+    console.log(pesron.family);
+    console.log(pesron.family.siblings);
+    console.log(pesron.family.siblings.sister);
+
+// varibale names
+// keywords dont use
+// must start by text
+// dont use -,space for variable name
+// use Underscore
+// meaningfullNames
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
